62fb6ffa38afa500ed1be03714e5068a08d75190
Ryan Burnette <ryan.burnette@gmail.com>
2014-11-25 23:52:16 -0500

    initial

---
+++ helpers.jquery.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var helpers
+  ;
+
+helpers.heightOfTallestOf = function ($els) {
+  var max = 0;
+  $els.each(function () {
+    max = $(this).outerHeight() > max ? $(this).outerHeight() : max;
+  });
+  return max;
+};
+
+helpers.getFloatedRows = function ($els) {
+  var rows = []
+    , curr = false
+    , prev = false
+    , i = 0
+    ;
+
+  $els.each(function () {
+    var $current = $(this).get(0);
+    curr = $current.getBoundingClientRect().top;
+    if ( prev && curr !== prev ) {
+      i = i + 1;
+    }
+    if ( rows[i] instanceof jQuery !== true ) {
+      rows[i] = $();
+    }
+    rows[i] = rows[i].add($(this));
+    prev = curr;
+  });
+
+  return rows;
+};
